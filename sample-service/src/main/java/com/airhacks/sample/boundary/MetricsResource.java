efa3bf373b31ce074c8bf5790d95ed7cba123e32
Adam Bien <abien@adam-bien.com>
2017-03-24 14:55:48 +0100

    value added to sample service

---
+++ sample-service/src/main/java/com/airhacks/sample/boundary/MetricsResource.java
@@ -0,0 +1,35 @@
+
+package com.airhacks.sample.boundary;
+
+import java.util.concurrent.atomic.AtomicLong;
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Path("metrics")
+@ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
+public class MetricsResource {
+
+    private AtomicLong counter = new AtomicLong();
+
+    @GET
+    public JsonObject metric() {
+        return Json.createObjectBuilder()
+                .add("application", "sample-service")
+                .add("component", "MetricsResource")
+                .add("units", "request")
+                .add("suffix", "total")
+                .add("value", counter.incrementAndGet())
+                .build();
+    }
+
+}
