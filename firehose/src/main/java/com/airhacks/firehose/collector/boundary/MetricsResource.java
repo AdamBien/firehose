166f973ce5a6859aab8a299bfcf2e4e635f651ab
Adam Bien <abien@adam-bien.com>
2017-03-25 10:57:45 +0100

    204 in metrics resource fixed

--- firehose/src/main/java/com/airhacks/firehose/collector/boundary/MetricsResource.java
+++ firehose/src/main/java/com/airhacks/firehose/collector/boundary/MetricsResource.java
@@ -2,6 +2,7 @@
 package com.airhacks.firehose.collector.boundary;
 
 import com.airhacks.firehose.collector.control.DataCollector;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -12,6 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -29,8 +31,15 @@ public class MetricsResource {
     @Path("{name}")
     public void metrics(@Suspended AsyncResponse response, @PathParam("name") String name) {
         response.setTimeout(1, TimeUnit.SECONDS);
-        dataCollector.fetchRemoteMetrics(name).map(m -> m.toMetric()).
-                ifPresent(response::resume);
+        Optional<String> metric = dataCollector.
+                fetchRemoteMetrics(name).
+                map(m -> m.toMetric());
+        if (metric.isPresent()) {
+            response.resume(metric.get());
+        } else {
+            response.resume(Response.noContent().build());
+        }
+
     }
 
 }
