a10d63cf8cf7b1f063451aae0632b8cae3dcb2bb
Adam Bien <abien@adam-bien.com>
2017-03-28 06:50:48 +0200

    filtering extracted

--- firehose/src/main/java/com/airhacks/firehose/configuration/control/EnvironmentVariables.java
+++ firehose/src/main/java/com/airhacks/firehose/configuration/control/EnvironmentVariables.java
@@ -1,7 +1,9 @@
 
 package com.airhacks.firehose.configuration.control;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Predicate;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -17,8 +19,14 @@ public class EnvironmentVariables {
     }
 
     public static Optional<JsonObject> getConfiguration(String configurationName) {
+
+        return getConfiguration(configurationName, e -> true);
+    }
+
+    public static Optional<JsonObject> getConfiguration(String configurationName, Predicate<Map.Entry<String, String>> filter) {
         JsonObjectBuilder builder = Json.createObjectBuilder();
-        System.getenv().entrySet().
+        System.getenv().
+                entrySet().
                 stream().
                 filter(e -> e.getKey().startsWith(configurationName + ".")).
                 forEach(e -> builder.add(skipPrefix(e.getKey()), e.getValue()));
