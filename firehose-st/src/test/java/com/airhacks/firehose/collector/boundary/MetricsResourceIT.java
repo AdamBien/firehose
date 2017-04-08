2ecc93486a5e15e2e392e9d828bd63334cde90b5
Adam Bien <abien@adam-bien.com>
2017-04-08 02:49:24 +0200

    ST for fetching all metrics implemented

--- firehose-st/src/test/java/com/airhacks/firehose/collector/boundary/MetricsResourceIT.java
+++ firehose-st/src/test/java/com/airhacks/firehose/collector/boundary/MetricsResourceIT.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import org.junit.Assume;
 import static org.junit.Assume.assumeThat;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,20 +27,37 @@ public class MetricsResourceIT {
     public void init() {
         this.client = ClientBuilder.newClient();
         this.tut = this.client.target("http://localhost:8080/firehose/resources/metrics");
+        Response pingResponse;
+        try {
+            pingResponse = this.client.target("http://localhost:8282/sample-service/resources/metrics").
+                    request().
+                    get();
+            assumeThat(pingResponse.getStatusInfo().getFamily(), is(Response.Status.Family.SUCCESSFUL));
+        } catch (Exception ex) {
+            Assume.assumeNoException(ex);
+            return;
+        }
+
+        JsonObject result = pingResponse.readEntity(JsonObject.class);
+        System.out.println("result = " + result);
+
     }
 
     @Test
     public void sampleServiceMetrics() {
-        Response pingResponse = this.client.target("http://localhost:8282/sample-service/resources/metrics").
-                request().
-                get();
-        assumeThat(pingResponse.getStatusInfo().getFamily(), is(Response.Status.Family.SUCCESSFUL));
-        JsonObject result = pingResponse.readEntity(JsonObject.class);
-        System.out.println("result = " + result);
 
         Response response = this.tut.path("sample-service").
                 request().
                 get();
+        assertThat(response.getStatus(), is(200));
+        String metric = response.readEntity(String.class);
+        assertNotNull(metric);
+        System.out.println("metric = " + metric);
+    }
+
+    @Test
+    public void allMetrics() {
+        Response response = this.tut.request().get();
         assertThat(response.getStatus(), is(200));
         String metric = response.readEntity(String.class);
         assertNotNull(metric);
