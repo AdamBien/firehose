ef5677a3ade6e16889c88fde4f7e1fd2d422a450
Adam Bien <abien@adam-bien.com>
2017-04-09 11:10:50 +0200

    text/plain and application/json metrics are requested

--- firehose/src/main/java/com/airhacks/firehose/collector/control/DataCollector.java
+++ firehose/src/main/java/com/airhacks/firehose/collector/control/DataCollector.java
@@ -3,13 +3,18 @@ package com.airhacks.firehose.collector.control;
 
 import com.airhacks.firehose.collector.entity.Metric;
 import com.airhacks.firehose.configuration.boundary.ConfigurationStore;
+import com.airhacks.firehose.extractor.control.NashornExtractor;
+import java.io.StringWriter;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonWriter;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
@@ -27,6 +32,9 @@ public class DataCollector {
     @Inject
     ConfigurationStore configurationStore;
 
+    @Inject
+    NashornExtractor extractor;
+
     static final String URI = "uri";
 
     @PostConstruct
@@ -36,16 +44,41 @@ public class DataCollector {
 
     public Optional<Metric> fetchRemoteMetrics(String metricsName) {
         Optional<JsonObject> optionalConfiguration = this.configurationStore.getConfiguration(metricsName);
+        Optional<Function<String, JsonObject>> optionalExtractor = this.extractor.getExtractor(optionalConfiguration);
+
         JsonObject configuration = optionalConfiguration.orElseThrow(() -> new MetricNotConfiguredException(metricsName));
         String extractedUri = extractUri(configuration);
         Response response = client.target(extractedUri).
-                request(MediaType.APPLICATION_JSON).
+                request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN).
                 get();
         if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
             return Optional.empty();
         }
-        JsonObject applicationData = response.readEntity(JsonObject.class);
-        return Optional.of(new Metric(configuration, applicationData));
+
+        Metric normalizedMetric = null;
+
+        if (response.getMediaType() == MediaType.APPLICATION_JSON_TYPE) {
+            JsonObject metric = response.readEntity(JsonObject.class);
+            normalizedMetric = optionalExtractor.
+                    map(f -> new Metric(configuration, f.apply(this.toString(metric)))).
+                    orElse(new Metric(configuration, metric));
+        } else {
+            String rawContent = response.readEntity(String.class);
+            normalizedMetric = optionalExtractor.
+                    map(f -> new Metric(configuration, f.apply(rawContent))).
+                    orElseThrow(() -> new NoScriptConfiguredForStringMetrics(metricsName));
+        }
+        return Optional.of(normalizedMetric);
+
+    }
+
+    String toString(JsonObject input) {
+        StringWriter stringWriter = new StringWriter();
+        try (JsonWriter jsonWriter = Json.createWriter(stringWriter)) {
+            jsonWriter.writeObject(input);
+            stringWriter.flush();
+        }
+        return stringWriter.getBuffer().toString();
     }
     
     public List<Metric> fetchRemoteMetrics() {
